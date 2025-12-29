01c359679756483e8d02c0d3b6210e8ecaca9bbe
HarryKim24 <tl9434@naver.com>
2025-12-29 10:33:23 +0900

    styles setting

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  const [number, setNumber] = useState(0);
-
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 50 }}>결과: {number}</Text>
-      <View style={{ flexDirection: 'row', gap: 10 }}>
-        <Button title='증가' onPress={() => setNumber(number + 1)} />
-        <Button title='감소' onPress={() => setNumber(number - 1)} />
-      </View>
+      <Text style={{ ...styles.text, color: 'blue' }}>리액트 네이티브</Text>
+      <Text style={{ ...styles.text }}>리액트 네이티브</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,4 +19,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 50,
+    color: 'red',
+    fontWeight: 'bold',
+  }
 });
